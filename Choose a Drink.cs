f413ac6d81207d39972f968f4e254bca96b9d59e
RumyanaRusimova <rumyanarusimova@abv.bg>
2017-06-02 01:40:19 +0300

    Create Choose a Drink.cs

---
+++ Choose a Drink.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Choose_a_Drink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var profession = Console.ReadLine();
+            switch (profession)
+            {
+                case "Athlete":
+                    Console.WriteLine("Water");
+                    break;
+                case "Businessman":                   
+                case "Businesswoman":
+                    Console.WriteLine("Coffee");
+                    break;
+                case "SoftUni Student":
+                    Console.WriteLine("Beer");
+                    break;
+                                  
+                default:
+                    Console.WriteLine("Tea");
+                    break;
+
+            }
+        }
+    }
+}
