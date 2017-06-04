4aac3a8b3f41f1234c0d143d4b5d1da54b7357a6
RumyanaRusimova <rumyanarusimova@abv.bg>
2017-06-04 20:59:02 +0300

    Create .Weather_Forecast .cs

---
+++ .Weather_Forecast .cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Weather_Forecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var num = long.Parse(Console.ReadLine());
+
+                if (num >= sbyte.MinValue && num < sbyte.MaxValue)
+                {
+                    Console.WriteLine("Sunny");
+                }
+                else if (num > int.MinValue && num < int.MaxValue)
+                {
+                    Console.WriteLine("Cloudy");
+                }
+                else 
+                {
+                    Console.WriteLine("Windy");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Rainy");
+            }
+
+        }
+
+    }
+}
+
